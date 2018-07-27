f01d5aa107dff22088aa4fe18695163336e522a7
soccert <talyagriffin2004@gmail.com>
2018-07-27 13:16:32 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -13,28 +13,28 @@ value_pennies = 0
 coins_value = {"quarters" => value_quarters, "dimes" => value_dimes, "nickels" => value_nickels, "pennies" => value_pennies}
 
 def least_coins(coins)
-  if coins% quarters == 0
+  if coins % quarters == 0
     value_quarters = coins / 25
     value_quarters = value_quarters.to_i
     coins_qd = value_quarters * 25
     coins = coins - coins_qd
   end
   
-  if coins% dimes >= 0
+  if coins% dimes == 0
     value_dimes = coins / 10
     value_dimes = value_dimes.to_i
     coins_dd = value_dimes * 10
     coins = coins - coins_dd
   end
   
-  if coins% nickels >= 0
+  if coins% nickels == 0
     value_nickels = coins / 5
     value_nickels = value_nickels.to_i
     coins_nd = value_nickels * 5
     coins = coins - coins_nd
   end
   
-  if coins% pennies >= 0
+  if coins% pennies == 0
     value_pennies = coins / 1
     value_pennies = value_pennies.to_i
     coins_pd = value_pennies * 1
